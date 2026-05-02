6a4c7b465e1a9b195c0ea657b4876f098b3d87d5
baotranpsy <baotranpsy@gmail.com>
2026-05-02 19:15:04 +0000

    Deploying to gh-pages from @ baotranpsy/academic@60038a66bed5bdeeaca99a49efe39a19fb8ffb47 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -465,13 +465,6 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/academic/projects/9_project/";
             },},{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%62%61%6F%74%72%61%6E%70%73%79@%67%6D%61%69%6C.%63%6F%6D", "_blank");
-        },
-      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
